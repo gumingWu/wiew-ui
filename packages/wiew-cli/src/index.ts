e2f3b737bcdff68d28af8dcfd259e745303dc112
gumingWu <15626185054@163.com>
2022-05-01 01:10:10 +0800

    完善command

--- packages/wiew-cli/src/index.ts
+++ packages/wiew-cli/src/index.ts
@@ -3,11 +3,30 @@ import path from "path";
 
 const cli = cac();
 
+/**
+ * cac tips
+ * 括号的使用
+ * 在command和option中都可以使用括号获取参数
+ * <>表示必填
+ * []表示可选
+ * 输入的参数都会在action中的回调函数的options中，是一个对象，{ '--': [], name: 'aaa' }
+ * 其中，可以在命令中，通过--继续传入不同的值，都可以在--: []中获取
+ * npx wiew-cli component --name hhh -- aaa bbb
+ * { '--': [ 'aaa', 'bbb' ], name: 'hhh' }
+ * 
+ * <>的值必填，不填会报错：CACError: missing required args for command `component <test>`
+ * command的括号值(<>/[])，可以在action中直接获取到
+ * .command("component <test>/[test]")
+ * .action((test, options) => {
+      console.log(test, options);
+    });
+ */
+
 cli
   .command("component")
-  .option("component", "Create Component")
-  .action((dir, options) => {
-    console.log(dir, options);
+  .option("--name [name]", "Component Name")
+  .action((options) => {
+    console.log(options);
   });
 
 cli
