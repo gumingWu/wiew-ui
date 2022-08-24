0c1ce41477e92af12072620041a7f90e70dd7a95
gumingWu <15626185054@163.com>
2022-08-25 01:43:01 +0800

    同上

--- docs/components/virtualScroll/index.md
+++ docs/components/virtualScroll/index.md
@@ -9,7 +9,14 @@ sidebar: VirtualScroll 虚拟列表
 :::demo
 ```vue
 <template>
-  <w-virtual-scroll></w-virtual-scroll>
+  <w-virtual-scroll :listData="d"></w-virtual-scroll>
 </template>
+
+<script setup>
+let d = [];
+for (let i = 0; i < 1000; i++) {
+  d.push({ id: i, value: i });
+}
+</script>
 ```
 :::
--- example/src/App.vue
+++ example/src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <WButton size="large">aaa</WButton>
+  <!-- <WButton size="large">aaa</WButton>
   <WButton size="middle">bbb</WButton>
   <WButton size="small">ccc</WButton>
   <WButton size="small" type="primary">ccc</WButton>
@@ -10,5 +10,7 @@
   <WButton size="small" status="danger">small</WButton>
   <WButton status="success">small</WButton>
   <WButton status="warning">small</WButton>
-  <WInput></WInput>
+  <WInput></WInput> -->
+  虚拟列表
+  <WVirtualScroll></WVirtualScroll>
 </template>
