7b4994dfdf55d81c57f33431c4dfc9f18dcd8b15
gumingWu <15626185054@163.com>
2022-08-24 11:14:01 +0800

    解决[Vue warn]: Invalid VNode type: Symbol(Text)问题

--- packages/wiew-ui/vite.config.ts
+++ packages/wiew-ui/vite.config.ts
@@ -11,6 +11,18 @@ export default defineConfig({
       name: 'wiew-ui',
       fileName: 'wiew-ui',
       formats: ['es', 'cjs', 'umd']
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   }
 })
