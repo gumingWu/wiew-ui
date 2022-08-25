bda41fa481205eb952ea921a6841e1ab80c48728
gumingWu <15626185054@163.com>
2022-08-26 00:45:07 +0800

    模态框基本样式，点击功能待修复

--- packages/components/modal/src/Modal.tsx
+++ packages/components/modal/src/Modal.tsx
@@ -1,12 +1,44 @@
 // step1. 创建组件.tsx
-import { defineComponent } from 'vue'
+import { defineComponent, Transition } from 'vue'
+import { ModalProps as props } from './props'
+import './Modal.less'
 
 export default defineComponent({
   name: 'WModal',
-  setup() {
+  props,
+  emits: ['close'],
+  setup(props, { emit, slots }) {
+    
 
     return () => (
-      <div>我是Modal</div>
+      <Transition name="modal">
+        {
+          props.show ? <div class="modal-mask">
+           <div class="modal-wrapper">
+             <div class="modal-container">
+               <div class="modal-header">
+                 {
+                   slots.header ? slots.header() : <div>Default Header</div>
+                 }
+               </div>
+               <div class="modal-body">
+                 {
+                   slots.default ? slots.default() : <div>Default Body</div>
+                 }
+               </div>
+               <div class="modal-footer">
+                 {
+                   slots.footer ? slots.footer() : <div>
+                    Default Footer
+                    <button onClick={emit('close')}>取消</button>
+                   </div>
+                 }
+               </div>
+             </div>
+           </div>
+         </div> : ''
+        }
+      </Transition>
     )
   }
 })
---
+++ packages/components/modal/src/types.ts
@@ -0,0 +1,4 @@
+import type { ExtractPropTypes } from 'vue'
+import { ModalProps } from './props'
+
+export type ModalPropsType = ExtractPropTypes<typeof ModalProps>
