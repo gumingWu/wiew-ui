de13bcb2f7131187c093f47e6cb04560e4a188d8
gumingWu <15626185054@163.com>
2022-09-01 12:12:52 +0800

    脚手架读取配置工程方法打通

--- packages/wiew-cli/src/config.ts
+++ packages/wiew-cli/src/config.ts
@@ -1,11 +1,10 @@
 import { parse } from 'path'
 import JoyCon from 'joycon'
-
-const { bundleRequire } = require('bundle-require')
+import jiti from 'jiti'
 
 export const DEFAULT_CONFIG_FILE = [
+  'wiew.config.ts',
   'wiew.config.js',
-  'wiew.config.ts'
 ]
 
 export async function loadWiewConfig(cwd: string) {
@@ -13,20 +12,20 @@ export async function loadWiewConfig(cwd: string) {
   const configPath = await joycon.resolve({
     files: DEFAULT_CONFIG_FILE,
     cwd,
-    stopDir: parse(cwd).root,
-    packageKey: 'wiew'
+    stopDir: parse(cwd).root
   })
 
   console.log(configPath);
-  if(configPath) {
-    const config = await bundleRequire({
-      filepath: configPath,
-    })
-    console.log(config);
-  }
+  const _require = jiti(cwd, { interopDefault: true, esmResolve: true })
+  const file = _require(configPath)
+  console.log(file);
 }
 
 // 占个坑，暂时做不出类型检查
-export function defineConfig(options) {
+export function defineConfig(options: Options): Options {
   return options
+}
+
+export type Options = {
+  name?: string
 }
--- packages/wiew-cli/tsup.config.ts
+++ packages/wiew-cli/tsup.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  entry: ['index.ts', 'cli.ts'],
-  format: ['esm', 'cjs'],
-  clean: true,
-  external: ['esbuild'],
-  outExtension({ format }) {
-    return {
-      js: `.${format}.js`
+export default defineConfig([
+  {
+    entry: ['index.ts'],
+    format: ['esm', 'cjs'],
+    dts: true,
+    outExtension({ format }) {
+      return {
+        js: `.${format}.js`
+      }
     }
+  },
+  {
+    entry: ['cli.ts'],
+    dts: false,
   }
-})
+])
