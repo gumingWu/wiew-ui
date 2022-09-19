0ae01f8d1addf48beb8c85073319c21488d53736
gumingWu <15626185054@163.com>
2022-09-20 01:18:01 +0800

    Tree组件大概展示

--- packages/components/tree/src/Tree.tsx
+++ packages/components/tree/src/Tree.tsx
@@ -1,14 +1,66 @@
-import { defineComponent } from "vue"
+import { defineComponent, toRefs, computed } from 'vue';
 import { TreeProps as props } from "./props"
-import type { TreePropsType } from "./types"
-import './Tree.css'
+import type { TreePropsType, TreeItem } from "./types"
+import './Tree.less'
+import { IconOpen } from './IconOpen';
+import { IconClose } from './IconClose'
 
 export default defineComponent({
   name: 'WTree',
   props,
   setup(props: TreePropsType) {
+    const { data } = toRefs(props)
+
+    const openedTree = (tree) => {
+      return tree.reduce((acc, item) => (
+        item.open
+          ? acc.concat(item, openedTree(item.children))
+          : acc.concat(item)
+      ), [])
+    }
+
+    const openData = computed(() => {
+      return openedTree(data.value)
+    })
+
+    const toggle = (item: TreeItem) => {
+      item.open = !item.open
+    }
+
+    const Indent = () => (
+      <span style="display: inline-block; width: 16px; height: 16px;"></span>
+    )
+
+    const renderNode = (item: TreeItem) => (
+      <div class="w-tree-node" style={{ paddingLeft: `${24 * (item.level - 1)}px` }}>
+        { item.children ?
+            item.open ? 
+              <IconClose class="mr-xs" onClick={() => toggle(item)} /> :
+                <IconOpen class="mr-xs" onClick={() => toggle(item)} /> :
+                  <Indent /> }
+        { item.label }
+      </div>
+    )
+
+    const renderTree = (tree: TreeItem[]) => {
+      return tree.map((item: TreeItem) => {
+        if(!item.children) {
+          return renderNode(item)
+        } else {
+          return (
+            <>
+              { renderNode(item) }
+              { renderTree(item.children) }
+            </>
+          )
+        }
+      })
+    }
+
     return () => (
-      <div class="w-tree">我是WTree</div>
+      <div class="w-tree">
+        { renderTree(openData.value) }
+      </div>
     )
   }
 })
--- packages/components/tree/src/props.ts
+++ packages/components/tree/src/props.ts
@@ -1,3 +1,9 @@
+import type { TreeItem } from "./types";
+import { PropType } from "vue";
+
 export const TreeProps = {
-  
-}
+  data: {
+    type: Array as PropType<TreeItem[]>,
+    default: () => [],
+  },
+};
